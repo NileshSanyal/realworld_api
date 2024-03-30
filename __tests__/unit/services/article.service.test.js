4fb42af9f716a7944bea38d63e15293e02e326c8
NileshSanyal <nil2take1@gmail.com>
2024-03-30 11:34:05 +0530

    Added unit test for saveArticle service function

--- __tests__/unit/services/article.service.test.js
+++ __tests__/unit/services/article.service.test.js
@@ -1,12 +1,13 @@
 const slugifyHelper = require('../../../helpers/slugify.helper');
-const currentUserService = require('../../../services/user.service');
 const { Article } = require('../../../models/article.model');
-const mockinGoose = require('mockingoose');
+const currentUserService = require('../../../services/user.service');
 
 const mockUser = {
   _id: "507f1f77bcf86cd799439011",
   email: "test@gmail.com"
 };
+
+const mockSlug = "sample-slug";
 
 const mockArticle = {
   title: "Sample article title",
@@ -16,12 +17,23 @@ const mockArticle = {
   author: "507f1f77bcf86cd799439011"
 };
 
-// Mock the Article model
-// jest.mock('../../../models/article.model');
+const mockUpdatedArticle = {
+  title: "Updated article title",
+  description: "Updated article description",
+  body: "Updated article body",
+  tagList: ['tag3', 'tag4'],
+  author: "507f1f77bcf86cd799439011"
+};
 
-// jest.mock('../../../models/article.model', () => ({
-//   save: jest.fn().mockResolvedValue(mockArticle)
-// }));
+// Mock the Article model
+jest.mock('../../../models/article.model', () => ({
+  Article: jest.fn().mockImplementation(() => ({
+    save: jest.fn().mockResolvedValue(mockArticle),
+    findOne: jest.fn().mockImplementation((slug) => {
+      slug == mockSlug ? Promise.resolve(mockArticle) : null
+    })
+  }))
+}));
 
 const slugifySpy = jest.spyOn(slugifyHelper, "slugify").mockReturnValue('mocked-slug');
 const currentUserSpy = jest.spyOn(currentUserService, "currentUser").mockResolvedValue(mockUser);
@@ -33,16 +45,30 @@ describe("Unit tests for article.service.js file", () => {
   describe("saveArticle tests", () => {
     test("If user can save an article", async () => {
       //Arrange
-      mockinGoose(Article).toReturn(mockArticle, 'save');
       const { saveArticle } = require("../../../services/article.service");
 
       //Act
       const result = await saveArticle(mockUser._id, mockArticle);
 
       //Assert
-      expect(result).toBe(mockArticle);
+      expect(result).toEqual(mockArticle);
       expect(slugifySpy).toHaveBeenCalledTimes(1);
       expect(slugifySpy).toHaveBeenCalledWith(mockArticle.title);
+      expect(currentUserSpy).toHaveBeenCalledTimes(1);
+      expect(currentUserSpy).toHaveBeenCalledWith(mockUser._id);
+    });
+  });
+  xdescribe("updateArticle tests", () => {
+    test("If user can update an existing article and user provides all data related to updated article", async () => {
+      //Arrange
+      const { updateArticle } = require("../../../services/article.service");
+      //Act
+      const result = await updateArticle(mockSlug, mockUser._id, mockUpdatedArticle);
+
+      //Assert
+      expect(result).toEqual(mockUpdatedArticle);
+      expect(slugifySpy).toHaveBeenCalledTimes(1);
+      expect(slugifySpy).toHaveBeenCalledWith(mockUpdatedArticle.title);
       expect(currentUserSpy).toHaveBeenCalledTimes(1);
       expect(currentUserSpy).toHaveBeenCalledWith(mockUser._id);
     });
